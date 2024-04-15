1cf1ad6da4a3e547b1b6b9f38fe4150195294942
jsum01 <djadjawltjr@gmail.com>
2024-04-15 20:32:37 +0900

    Update : 학생정보 정규식에 맞춰 예외처리 프로그램 작성

---
+++ src/java24mid/except_student/InfoException.java
@@ -0,0 +1,21 @@
+package java24mid.except_student;
+
+
+public class InfoException extends Exception {
+
+    /**
+     * message: "형식에 맞지 않는 입력입니다
+     */
+    public InfoException() {
+        super("형식에 맞지 않는 입력입니다.");
+    }
+
+    /**
+     * 사용자 지정 에러 메세지를 출력합니다.
+     * @param message
+     */
+    public InfoException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/java24mid/except_student/RegexIF.java
@@ -0,0 +1,8 @@
+package java24mid.except_student;
+
+public interface RegexIF {
+
+    public static final String nameRegex = "^[a-zA-Z가-힣]*$"; // 이름 정규식
+    public static final String juminRegex = "^[0-9]{6}-[0-9]{7}"; // 주민번호 정규식
+
+}
